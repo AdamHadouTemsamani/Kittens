c16588326f620db9de87d4dd6b5ba0792b1e143f
Adam Hadou Temsamani <94063688+AdamHadouTemsamani@users.noreply.github.com>
2024-04-26 15:33:13 +0200

    init

---
+++ Kittens/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='header'>
+
+      </div>
+      <div className='tweets'>
+
+      </div>
+      <div className='tweetInput'></div>
+    </>
+  )
+}
+
+export default App
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Kittens
- A twitter clone for cats! made in React
+ A twitter clone for cats!
+
+ The code is written in Javascript using React.
+
+ Relevant Links:
+ * https://rxdb.info/articles/react-database.html
+ * https://github.com/PhilipFlyvholm/UXWP-Live-coding
